d55a5766d5dddbac3bfe316faa31fcec6b71d475
Hendi Santika <hendisantika@gmail.com>
2017-11-07 06:16:12 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbooti18n/SpringBootI18nApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbooti18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootI18nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootI18nApplication.class, args);
+	}
+}
